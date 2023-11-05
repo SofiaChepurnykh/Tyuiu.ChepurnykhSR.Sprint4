3ed7ea51ee865d900b2a90bb8e241321c58e343a
Tyuiu.ChepurnykhSR@outlook.com <winxsofa@bk.ru>
2023-11-05 23:00:33 +0500

    Task3

---
+++ Tyuiu.ChepurnykhSR.Sprint4.Task3.V30.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ChepurnykhSR.Sprint4.Task3.V30.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ChepurnykhSR.Sprint4.Task3.V30.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("80c7a6ed-5002-435f-9a61-b05f61e311bb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ChepurnykhSR.Sprint4.Task3.V30/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ChepurnykhSR.Sprint4.Task3.V30.Lib;
+
+namespace Tyuiu.ChepurnykhSR.Sprint4.Task3.V30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+
+            Console.Title = "Спринт #4 | Выполнила: Чепурных С.Р. | ИСТНб-23-1";
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("* Спринт #4                                                                            *");
+            Console.WriteLine("* Тема: Обработка структурных типов                                                    *");
+            Console.WriteLine("* Задание #3                                                                           *");
+            Console.WriteLine("* Вариант #30                                                                          *");
+            Console.WriteLine("* Выполнила: Чепурных С.Р. | ИСТНб-23-1                                                *");
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                             *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный статическими        *");
+            Console.WriteLine("* значениями в диапазоне от 1 до 6. Найдите максимальный элемент                       *");
+            Console.WriteLine("* в третьей строке массива.                                                            *");
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                     *");
+            Console.WriteLine("****************************************************************************************");
+
+
+
+
+            int[,] arr =
+            {
+                {2,4,3,5,1 },
+                {6,6,1,2,6 },
+                {3,3,2,1,5 },
+                {6,4,1,3,3 },
+                {5,1,1,6,4 }
+            };
+
+
+            Console.WriteLine("Массив:");
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                           *");
+            Console.WriteLine("****************************************************************************************");
+
+            Console.WriteLine("Максимальный элемент в 3 строке массива = " + ds.Calculate(arr));
+
+            Console.ReadLine();
+        }
+    }
+}
