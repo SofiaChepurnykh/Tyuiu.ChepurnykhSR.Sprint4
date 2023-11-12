26919f64a1f77e16440ff34a401caf4b6b6451fe
Tyuiu.ChepurnykhSR@outlook.com <winxsofa@bk.ru>
2023-11-12 22:56:36 +0500

    Task5

---
+++ Tyuiu.ChepurnykhSR.Sprint4.Task5.V15/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ChepurnykhSR.Sprint4.Task5.V15.Lib;
+
+namespace Tyuiu.ChepurnykhSR.Sprint4.Task5.V15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            DataService dataService = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнила: Чепурных С. Р. | ИСТНб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Обработка структурных типов                                       *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #15                                                             *");
+            Console.WriteLine("* Выполнила: Чепурных С. Р. | ИСТНб-23-1                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* статическими значениями в диапазоне от 3 до 8. Найдите максимальный     *");
+            Console.WriteLine("* элемент во второй строке массива.                                       *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите количество строк в массиве: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите количество столбцов в массиве: ");
+            int columns = Convert.ToInt32(Console.ReadLine());
+
+            int[,] numsArray = new int[rows, columns];
+
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    numsArray[i, j] = rnd.Next(-6, 4);
+                }
+            }
+
+            Console.Write("Массив:{ ");
+            for (int i = 0; i < numsArray.GetUpperBound(0) + 1; i++)
+            {
+                if (i != 0) { Console.Write("\t "); }
+                Console.Write("{");
+                for (int j = 0; j < numsArray.Length / (numsArray.GetUpperBound(0) + 1); j++)
+                {
+                    Console.Write(numsArray[i, j]);
+                    if (j != numsArray.GetLength(0) - 1) { Console.Write(", "); }
+                }
+                Console.Write("}");
+                if (i != numsArray.GetLength(0) - 1) { Console.WriteLine(","); }
+            }
+            Console.WriteLine("}");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = dataService.Calculate(numsArray);
+
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
